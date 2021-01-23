After getting tired of fighting pandas, I converted as much of the code as possible to the Pandas vectorization methods, fixed a bug in deltaNope2 sign as a result as well.

scripts/differential_analysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import *
 import pandas as pd
 from scipy import stats
 from dateutil import parser
@@ -11,44 +12,47 @@ import statistics
 DATA_PATH = '../processed_data/'
 
 df = pd.read_csv(DATA_PATH + 'allNOPEDataCombined.csv')
-
-deltaNope = 0.0
-deltaNope2 = 0.0
-deltaPrice = 0.0
-lastNope = 0.0
-lastDNope = 0.0
-lastDNope2 = 0.0
-lastPrice = 0.0
-dPriceLag = []
-filterDatetime = pd.to_datetime('2020-01-02 9:35')
-endFilterDatetime = pd.to_datetime('2020-01-02 16:35')
-
 df['timestamp'] = df['timestamp'].apply(lambda x: pd.to_datetime(x))
+
+dPriceLag = []
+monthDateTime = pd.to_datetime('2020-01-02 9:35')
+monthDateTime = monthDateTime.replace(day=1)
+filterDatetime = pd.to_datetime('2020-01-02 9:35')
+endFilterDatetime = pd.to_datetime('2020-02-02 16:35')
+breakFilterDateTime = pd.to_datetime('2020-02-02 16:35')
+
+# we need to setup our shifted data columns
+df['NOPE_allVolume'] = df['NOPE_allVolume'].apply(lambda x: x*100.0)
+df['NOPE_allVolumePrev'] = df['NOPE_allVolume'].shift()
+
+df['deltaNope'] = df.apply(lambda x: x['NOPE_allVolume'] - x['NOPE_allVolumePrev'], axis=1)
+df['deltaNope'].fillna(0.0 ,inplace = True)
+df['deltaNopePrev'] = df['deltaNope'].shift()
+
+df['deltaNope2'] = df.apply(lambda x: x['deltaNope'] - x['deltaNopePrev'], axis=1)
+df['deltaNope2'].fillna(0.0 ,inplace = True)
+df['deltaNope2Prev'] = df['deltaNope2'].shift()
+
+df['active_underlying_price_prev'] = df['active_underlying_price'].shift()
+
+df['deltaPrice'] = df.apply(lambda x: x['active_underlying_price'] - x['active_underlying_price_prev'], axis=1)
+df['deltaPrice'].fillna(0.0 ,inplace = True)
+
+df['deltaPrice+5'] = df['deltaPrice'].shift(-1)
+df['deltaPrice+5'].fillna(0.0 ,inplace = True)
+
+df['deltaPrice+10'] = df['deltaPrice'].shift(-2)
+df['deltaPrice+10'].fillna(0.0 ,inplace = True)
+
+df['dN-dN2'] = df.apply(lambda x: x['deltaNope'] - (0.5*x['deltaNope2Prev'] - 0.5*x['deltaNope2']), axis=1)
+df['dN-dN2'].fillna(0.0 ,inplace = True)
+print(df)
 
 for index, row in df.iterrows():
     #print(index, row)
     if (index == 0):
-        df.at[index,'deltaNope'] = 0.0
-        df.at[index,'deltaPrice'] = 0.0
-        df.at[index,'deltaNope2'] = 0.0
-        df.at[index,'dN-dN2'] = 0.0
-        df.at[index,'corr'] = 0.0
-        df.at[index,'corrLag'] = 0.0
-        df.at[index,'corrLag_2'] = 0.0
-        df.at[index,'corrRaw'] = 0.0
-        df.at[index,'corrD2'] = 0.0
-        df.at[index,'corrD2Lag'] = 0.0
-        df.at[index,'corrD2Lag_2'] = 0.0
-        df.at[index,'corrdN_sub_dN2'] = 0.0
-        df.at[index,'corrdN_sub_dN2_lag'] = 0.0
-        df.at[index,'corrdN_sub_dN2_lag_2'] = 0.0
         filterDatetime = row['timestamp']
         endFilterDatetime = row['timestamp'].replace(hour=16)
-        print('Processing entry range: ', filterDatetime, ' ', endFilterDatetime)
-        lastNope = row['NOPE_allVolume']
-        lastPrice = row['active_underlying_price']
-        lastDNope = 0.0
-        lastDNope2 = 0.0
         continue
     else:
         # ignore the first entry in each day, as we just want to compare the data within a single day, and not the end
@@ -57,94 +61,69 @@ for index, row in df.iterrows():
             # build the day range filter begin and end
             filterDatetime = row['timestamp']
             endFilterDatetime = row['timestamp'].replace(hour=16)
-            print('Processing entry range: ' , filterDatetime, ' ', endFilterDatetime)
-            lastNope = row['NOPE_allVolume']
-            lastPrice = row['active_underlying_price']
-            lastDNope = 0.0
-            lastDNope2 = 0.0
+            monthDateTime = row['timestamp'].replace(day=1)
+            print('Processing entry range: ', filterDatetime, ' ', endFilterDatetime)
             continue
 
-        rawNope = row['NOPE_allVolume']
-        deltaNope = lastNope - rawNope
-        deltaPrice = lastPrice - row['active_underlying_price']
-        deltaNope2 = lastDNope - deltaNope
-        print("NOPE: ", rawNope)
-        if(rawNope > 25 or rawNope < -25):
-            #print(deltaNope, deltaPrice)
-            df.at[index, 'deltaNope'] = deltaNope
-            df.at[index, 'deltaPrice'] = deltaPrice
-            df.at[index, 'deltaNope2'] = deltaNope2
 
-            # use a split interpolate on the second order to function as the modeled second order
-            # this is a reduction of cubic spline interpolation used in forward forecasting signal processing
-            df.at[index, 'dN-dN2'] = deltaNope - (0.5*lastDNope2 - 0.5*deltaNope2)
+        # use a split interpolate on the second order to function as the modeled second order
+        # this is a reduction of cubic spline interpolation used in forward forecasting signal processing
+        #df.at[index, 'dN-dN2'] = deltaNope - (0.5*lastDNope2 - 0.5*deltaNope2)
 
-            # process data within our day separately
-            #dNope = df[(df['timestamp'] >= filterDatetime) & (df['timestamp'] < endFilterDatetime)]['deltaNope'].to_numpy()
-            #dNope2 = df[(df['timestamp'] >= filterDatetime) & (df['timestamp'] < endFilterDatetime)]['deltaNope2'].to_numpy()
-            #dN_sub_dN2 = df[(df['timestamp'] >= filterDatetime) & (df['timestamp'] < endFilterDatetime)]['dN-dN2'].to_numpy()
-            #dPrice = df[(df['timestamp'] >= filterDatetime) & (df['timestamp'] < endFilterDatetime)]['deltaPrice'].to_numpy()
-            #dNopeRaw = df[(df['timestamp'] >= filterDatetime) & (df['timestamp'] < endFilterDatetime)]['NOPE_allVolume'].to_numpy()
-            dNope = df['deltaNope'].to_numpy()
-            dNope2 = df['deltaNope2'].to_numpy()
-            dN_sub_dN2 = df['dN-dN2'].to_numpy()
-            dPrice = df['deltaPrice'].to_numpy()
-            dNopeRaw = df['NOPE_allVolume'].to_numpy()
+        # process data up to the latest data entry
+        dNope = df[(df['timestamp'] <= filterDatetime) & (df['timestamp'] >= monthDateTime)]['deltaNope'].to_numpy()
+        # where_are_NaNs = isnan(dNope)
+        # dNope[where_are_NaNs] = 0.0
 
-            # shift the data back by 1
-            dPriceLag = np.delete(dPrice, 0, 0)
-            dPriceLag = np.insert(dPriceLag, dPriceLag.size - 1, 0)
+        dNope2 = df[(df['timestamp'] <= filterDatetime) & (df['timestamp'] >= monthDateTime)]['deltaNope2'].to_numpy()
+        # where_are_NaNs = isnan(dNope2)
+        # dNope2[where_are_NaNs] = 0.0
 
-            # shift the data back by 2
-            dPriceLag2 = np.delete(dPrice, 0, 0)
-            dPriceLag2 = np.delete(dPriceLag2, 0, 0)
-            dPriceLag2 = np.insert(dPriceLag2, dPriceLag2.size - 1, 0)
-            dPriceLag2 = np.insert(dPriceLag2, dPriceLag2.size - 1, 0)
+        dN_sub_dN2 = df[(df['timestamp'] <= filterDatetime) & (df['timestamp'] >= monthDateTime)]['dN-dN2'].to_numpy()
+        # where_are_NaNs = isnan(dN_sub_dN2)
+        # dN_sub_dN2[where_are_NaNs] = 0.0
 
-            # compute our various correlations
-            dN_dP = stats.spearmanr(dNope, dPrice)
-            df.at[index, 'corr'] = dN_dP[0]
-            #print("dNope p Value: ", str(dN_dP[1]))
-            df.at[index, 'corrLag'] = stats.spearmanr(dNope, dPriceLag)[0]
-            df.at[index, 'corrLag_2'] = stats.spearmanr(dNope, dPriceLag2)[0]
-            df.at[index, 'corrRaw'] = stats.spearmanr(dNope, dNopeRaw)[0]
+        dPrice = df[(df['timestamp'] <= filterDatetime) & (df['timestamp'] >= monthDateTime)]['deltaPrice'].to_numpy()
+        # where_are_NaNs = isnan(dPrice)
+        # dPrice[where_are_NaNs] = 0.0
 
-            dN2_dP = stats.spearmanr(dNope2, dPrice)
-            df.at[index, 'corrD2'] = dN2_dP[0]
-            #print("dNope2 p Value: ", str(dN2_dP[1]))
-            df.at[index, 'corrD2Lag'] = stats.spearmanr(dNope2, dPriceLag)[0]
-            df.at[index, 'corrD2Lag_2'] = stats.spearmanr(dNope2, dPriceLag2)[0]
+        dNopeRaw = df[(df['timestamp'] <= filterDatetime) & (df['timestamp'] >= monthDateTime)]['NOPE_allVolume'].to_numpy()
+        # where_are_NaNs = isnan(dNopeRaw)
+        # dNopeRaw[where_are_NaNs] = 0.0
 
-            dN_dN2_dP = stats.spearmanr(dN_sub_dN2, dPrice)
-            df.at[index, 'corrdN_sub_dN2'] = dN_dN2_dP[0]
-            #print("dNope - dNope2 p Value: ", str(dN_dN2_dP[1]))
+        dPrice_p5 = df[(df['timestamp'] <= filterDatetime) & (df['timestamp'] >= monthDateTime)]['deltaPrice+5'].to_numpy()
 
+        dPrice_p10 = df[(df['timestamp'] <= filterDatetime) & (df['timestamp'] >= monthDateTime)]['deltaPrice+10'].to_numpy()
+        # dNope = df['deltaNope'].to_numpy()
+        # dNope2 = df['deltaNope2'].to_numpy()
+        # dN_sub_dN2 = df['dN-dN2'].to_numpy()
+        # dPrice = df['deltaPrice'].to_numpy()
+        # dNopeRaw = df['NOPE_allVolume'].to_numpy()
 
-            dN_dN2_dP_t_plus_5 = stats.spearmanr(dN_sub_dN2, dPriceLag)
-            df.at[index, 'corrdN_sub_dN2_lag'] = dN_dN2_dP_t_plus_5[0]
-            #print("dNope - dNope2 T+5 p Value: ", str(dN_dN2_dP_t_plus_5[1]))
-            df.at[index, 'corrdN_sub_dN2_lag_2'] = stats.spearmanr(dN_sub_dN2, dPriceLag2)[0]
-        elif(rawNope < 25 and rawNope > -25 and index > 2):
-            df.at[index, 'deltaNope'] = df.at[index-1, 'deltaNope']
-            df.at[index, 'deltaPrice'] = df.at[index-1, 'deltaPrice']
-            df.at[index, 'deltaNope2'] = df.at[index-1, 'deltaNope2']
-            df.at[index, 'dN-dN2'] = df.at[index-1, 'dN-dN2']
+        df.at[index, 'corrLag'] = stats.spearmanr(dNope, dPrice_p5)[0]
+        df.at[index, 'corrD2Lag'] = stats.spearmanr(dNope2, dPrice_p5)[0]
+        dN_dN2_dP_t_plus_5 = stats.spearmanr(dN_sub_dN2, dPrice_p5)
+        df.at[index, 'corrdN_sub_dN2_lag'] = dN_dN2_dP_t_plus_5[0]
 
-            df.at[index, 'corr'] = df.at[index-1, 'corr']
-            df.at[index, 'corrLag'] = df.at[index-1, 'corrLag']
-            df.at[index, 'corrLag_2'] = df.at[index-1, 'corrLag_2']
-            df.at[index, 'corrRaw'] = df.at[index-1, 'corrRaw']
-            df.at[index, 'corrD2'] = df.at[index-1, 'corrD2']
-            df.at[index, 'corrD2Lag'] = df.at[index-1, 'corrD2Lag']
-            df.at[index, 'corrD2Lag_2'] = df.at[index-1, 'corrD2Lag_2']
-            df.at[index, 'corrdN_sub_dN2'] = df.at[index-1, 'corrdN_sub_dN2']
-            df.at[index, 'corrdN_sub_dN2_lag'] = df.at[index-1, 'corrdN_sub_dN2_lag']
-            df.at[index, 'corrdN_sub_dN2_lag_2'] = df.at[index-1, 'corrdN_sub_dN2_lag_2']
+        df.at[index, 'corrLag_2'] = stats.spearmanr(dNope, dPrice_p10)[0]
+        df.at[index, 'corrD2Lag_2'] = stats.spearmanr(dNope2, dPrice_p10)[0]
+        df.at[index, 'corrdN_sub_dN2_lag_2'] = stats.spearmanr(dN_sub_dN2, dPrice_p10)[0]
 
-    lastNope = row['NOPE_allVolume']
-    lastPrice = row['active_underlying_price']
-    lastDNope = deltaNope
-    lastDNope2 = deltaNope2
+        #print(dNope, dPrice, dPriceLag, dPriceLag2)
+
+        # compute our various correlations
+        dN_dP = stats.spearmanr(dNope, dPrice)
+        df.at[index, 'corr'] = dN_dP[0]
+        df.at[index, 'corrRaw'] = stats.spearmanr(dNope, dNopeRaw)[0]
+
+        dN2_dP = stats.spearmanr(dNope2, dPrice)
+        df.at[index, 'corrD2'] = dN2_dP[0]
+
+        dN_dN2_dP = stats.spearmanr(dN_sub_dN2, dPrice)
+        df.at[index, 'corrdN_sub_dN2'] = dN_dN2_dP[0]
+
+        # if(row['timestamp'] > breakFilterDateTime):
+        #     break
 
 fig, axs = plt.subplots(3, 1)
 fig.suptitle('Spearman R Rank Correlation \n SPY Intra-day deltaNope vs. deltaPrice August 2020')